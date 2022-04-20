caf978cb2e1483affca5788c205a3234a8b267f9
vandana l <vandana.developer2019@gmail.com>
2022-04-21 00:21:38 +1200

    Add project files.

---
+++ RandomNumberDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Random random  = new Random();
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine(random.Next(10)); // maxvalue is 10
+}
+
+Console.WriteLine("random numbers");
+
+for (int i = 0; i < 5; i++)
+{
+    // inclusive of 5 and less than the max value
+    // Console.WriteLine(random.Next(5,10)); // maxvalue is 10
+    Console.WriteLine(random.NextDouble() * 2);
+}
+
+List<PersonModel> people = new List<PersonModel>
+{
+    new PersonModel {FirstName ="Tim"},
+    new PersonModel{FirstName ="Sue"},
+    new PersonModel {FirstName = "Mary"},
+    new PersonModel {FirstName ="George"},
+    new PersonModel {FirstName = "Jane"},
+    new PersonModel {FirstName = "Nancy"},
+    new PersonModel {FirstName = "Bob"}
+};
+
+
+Console.WriteLine("** shuffle a shorter list **");
+
+//var sortedPeople = people.OrderBy(x => x.FirstName);
+// shuffle a small list
+var sortedPeople = people.OrderBy(x => random.Next());
+
+foreach (var p in sortedPeople)
+{
+    Console.WriteLine(p.FirstName);
+}
+
+
+Console.ReadLine();
+
+
+
+
+public class PersonModel
+{
+    public string FirstName { get; set; }
+}
